48169cbb65257d132d4696dce33165d0d5d09e37
Narazaka <info@narazaka.net>
2018-06-17 09:10:19 +0900

    IF変更

--- lib/shiori-encode-layer.ts
+++ lib/shiori-encode-layer.ts
@@ -3,23 +3,24 @@ import { Shiori } from "shioriloader";
 
 export interface RawShiori {
     load(dirpath: string): Promise<number>;
-    request(request: string | Buffer): Promise<string | Buffer>;
+    request(request: ArrayBuffer): Promise<ArrayBuffer>;
     unload(): Promise<number>;
 }
 
 export class ShioriEncodeLayer implements Shiori {
-    private static encodeRequest(content: string | Buffer) {
+    private static encodeRequest(content: string) {
         return ShioriEncodeLayer.encode(content, ShioriEncodeLayer.getCharset(content));
     }
 
-    private static decodeResponse(content: string | Buffer) {
+    private static decodeResponse(content: ArrayBuffer) {
         return ShioriEncodeLayer.decode(content, ShioriEncodeLayer.getCharset(content));
     }
 
-    private static getCharset(content: string | Buffer) {
-        const charsetMatch =
-            (content instanceof Buffer ? content.toString("ascii") : content).match(/Charset: ([^\x0d\x0a]+)/i);
-        const charset = charsetMatch ? charsetMatch[1] : "AUTO";
+    private static getCharset(content: string | ArrayBuffer) {
+        const charsetMatch = typeof content === "string" ? content.match(/Charset: ([^\x0d\x0a]+)/i) : null;
+        /*const charsetMatch =
+            (content instanceof Buffer ? content.toString("ascii") : content).match(/Charset: ([^\x0d\x0a]+)/i);*/
+        const charset = charsetMatch ? charsetMatch[1] : "Shift_JIS";
         return ShioriEncodeLayer.charsetType(charset);
     }
 
@@ -30,20 +31,12 @@ export class ShioriEncodeLayer implements Shiori {
         return "AUTO";
     }
 
-    private static encode(content: string | Buffer, charset: Encoding.Encoding) {
-        return Encoding.convert(content, {
-            from: "UNICODE",
-            to: charset,
-            type: "string",
-        }) as string;
+    private static encode(content: string, charset: Encoding.Encoding) {
+        return new Uint8Array(Encoding.convert(content, {to: charset, type: "array"}) as number[]).buffer as ArrayBuffer;
     }
 
-    private static decode(content: string | Buffer, charset: Encoding.Encoding) {
-        return Encoding.convert(content, {
-            from: charset,
-            to: "UNICODE",
-            type: "string",
-        }) as string;
+    private static decode(content: ArrayBuffer, charset: Encoding.Encoding) {
+        return Encoding.convert(new Uint8Array(content), {to: "UNICODE", from: charset, type: "string"}) as string;
     }
 
     childShiori: RawShiori;
@@ -60,7 +53,7 @@ export class ShioriEncodeLayer implements Shiori {
         return this.childShiori.unload();
     }
 
-    async request(request: string | Buffer) {
+    async request(request: string) {
         return ShioriEncodeLayer.decodeResponse(
             await this.childShiori.request(
                 ShioriEncodeLayer.encodeRequest(request),
--- test/basic.ts
+++ test/basic.ts
@@ -1,39 +1,35 @@
 import * as Encoding from "encoding-japanese";
 import * as assert from "power-assert";
-import { Shiori } from "shioriloader";
 import * as sinon from "sinon";
-import { ShioriEncodeLayer } from "../lib/shiori-encode-layer";
+import { ShioriEncodeLayer, RawShiori } from "../lib/shiori-encode-layer";
 
 const crlf = "\x0d\x0a";
 
-const encode = (content: string, charset: Encoding.Encoding) =>
-    Encoding.convert(content, {to: charset, type: "string"}) as string;
-
-class ChildShiori implements Shiori {
+class ChildShiori implements RawShiori {
     static requestString(charset: string, value: string) {
         return `SHIORI/3.0 200 OK${crlf}Charset: ${charset}${crlf}Value: ${value}${crlf}${crlf}`;
     }
 
     charset: string;
     value: string;
-    encodedValue: string;
 
-    constructor(charset: string, value: string, charset2: Encoding.Encoding) {
+    constructor(charset: string, value: string) {
         this.charset = charset;
         this.value = value;
-        this.encodedValue = encode(value, charset2);
     }
 
     async load(_: string) { return 1; }
 
-    async request(_: string) {
-        return ChildShiori.requestString(this.charset, this.encodedValue);
+    async request(_: ArrayBuffer) {
+        const val = Encoding.convert(ChildShiori.requestString(this.charset, this.value), {to: "SJIS", from: "UNICODE", type: "array"});
+        console.error(val);
+        return new Uint8Array(val).buffer as ArrayBuffer;
     }
 
     async unload() { return 1; }
 }
 
-const sjisChildShiori = () => new ChildShiori("Shift_JIS", "能勢電鉄と表現", "SJIS");
+const sjisChildShiori = () => new ChildShiori("Shift_JIS", "能勢電鉄と表現");
 
 describe("ShioriEncodeLayer", () => {
     describe("load()", () => {
@@ -54,8 +50,9 @@ describe("ShioriEncodeLayer", () => {
             const requestSpy = sinon.spy(childShiori, "request");
             const request = `GET SHIORI/3.0${crlf}Charset: Shift_JIS${crlf}ID: ソビエトロシア${crlf}${crlf}`;
             const response = await shiori.request(request);
+            console.log();
+            assert.deepEqual(requestSpy.firstCall.args[0], new Buffer(Encoding.convert(request, "SJIS")).buffer);
             assert(response === ChildShiori.requestString(childShiori.charset, childShiori.value));
-            assert(requestSpy.calledOnceWith(encode(request, "SJIS")));
         });
     });
 
