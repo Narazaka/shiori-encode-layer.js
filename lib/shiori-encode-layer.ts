10ea4484394f758106d266c5160fe012f84233b6
Narazaka <info@narazaka.net>
2018-06-17 10:30:33 +0900

    encode support implove

--- lib/shiori-encode-layer.ts
+++ lib/shiori-encode-layer.ts
@@ -7,32 +7,70 @@ export interface RawShiori {
     unload(): Promise<number>;
 }
 
+const charsetHeaderCodes = [0x0a, 0x63, 0x68, 0x61, 0x72, 0x73, 0x65, 0x74, 0x3a, 0x20]; // "\x0acharset: "
+const charsetHeaderCodesLength = charsetHeaderCodes.length;
+const charsetHeaderAlphaStart = 1;
+const charsetHeaderAlphaEnd = 8; // ":"の位置
+const cr = 0x0d;
+
 export class ShioriEncodeLayer implements Shiori {
     private static encodeRequest(content: string) {
-        return ShioriEncodeLayer.encode(content, ShioriEncodeLayer.getCharset(content));
+        return ShioriEncodeLayer.encode(content, ShioriEncodeLayer.getStringCharset(content));
     }
 
     private static decodeResponse(content: ArrayBuffer) {
-        return ShioriEncodeLayer.decode(content, ShioriEncodeLayer.getCharset(content));
+        return ShioriEncodeLayer.decode(content, ShioriEncodeLayer.getArrayBufferCharset(content));
     }
 
-    private static getCharset(content: string | ArrayBuffer) {
-        const charsetMatch = typeof content === "string" ? content.match(/Charset: ([^\x0d\x0a]+)/i) : null;
-        /*const charsetMatch =
-            (content instanceof Buffer ? content.toString("ascii") : content).match(/Charset: ([^\x0d\x0a]+)/i);*/
-        const charset = charsetMatch ? charsetMatch[1] : "Shift_JIS";
+    private static getStringCharset(content: string) {
+        const charsetMatch = content.match(/Charset: ([^\x0d\x0a]+)/i);
+        if (!charsetMatch) return "AUTO";
+        const charset = charsetMatch[1];
         return ShioriEncodeLayer.charsetType(charset);
+    }
+
+    private static getArrayBufferCharset(content: ArrayBuffer) {
+        const chars = new Uint8Array(content);
+        let headerDetectIndex = 0;
+        let charsetHeaderValueStart = -1;
+        for (let charIndex = 0; charIndex < chars.length; ++charIndex) {
+            let char = chars[charIndex];
+            if (charsetHeaderAlphaStart <= headerDetectIndex && headerDetectIndex < charsetHeaderAlphaEnd) {
+                // 小文字へ変換
+                char |= 0x20; // tslint:disable-line no-bitwise
+            }
+            if (char === charsetHeaderCodes[headerDetectIndex]) {
+                ++headerDetectIndex;
+            } else {
+                headerDetectIndex = 0;
+            }
+            if (headerDetectIndex === charsetHeaderCodesLength) {
+                charsetHeaderValueStart = charIndex + 1;
+                break;
+            }
+        }
+        if (charsetHeaderValueStart < 0) return "AUTO";
+        const charsetHeaderValueCodes = [];
+        for (let charIndex = charsetHeaderValueStart; charIndex < chars.length; ++charIndex) {
+            const char = chars[charIndex];
+            if (char === cr) break;
+            charsetHeaderValueCodes.push(char);
+        }
+        return ShioriEncodeLayer.charsetType(String.fromCharCode(...charsetHeaderValueCodes));
     }
 
     private static charsetType(charset: string) {
         const lowerCharset = charset.toLowerCase();
         if (lowerCharset === "shift_jis") return "SJIS";
+        if (lowerCharset === "cp932") return "SJIS";
         if (lowerCharset === "utf-8") return "UTF8";
         return "AUTO";
     }
 
     private static encode(content: string, charset: Encoding.Encoding) {
-        return new Uint8Array(Encoding.convert(content, {to: charset, type: "array"}) as number[]).buffer as ArrayBuffer;
+        return new Uint8Array(
+            Encoding.convert(content, {to: charset, from: "UNICODE", type: "array"}) as number[],
+        ).buffer as ArrayBuffer;
     }
 
     private static decode(content: ArrayBuffer, charset: Encoding.Encoding) {
